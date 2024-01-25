310bb269787102a3408102378f20bdf9b7855738
vicat47 <vicat47@qq.com>
2024-01-25 10:27:25 +0800

    ✨: 添加⚙️，读取✉️

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log("hello world!");
+const config = require('config');
+const { ImapFlow } = require('imapflow');
+const cfg = config.get("mail");
+const client = new ImapFlow(cfg);
+
+const main = async () => {
+    // Wait until client connects and authorizes
+    await client.connect();
+
+    // Select and lock a mailbox. Throws if mailbox does not exist
+    let lock = await client.getMailboxLock('INBOX');
+    try {
+        // fetch latest message source
+        // client.mailbox includes information about currently selected mailbox
+        // "exists" value is also the largest sequence number available in the mailbox
+        let message = await client.fetchOne(client.mailbox.exists, { source: true });
+        console.log(message.source.toString());
+
+        // list subjects for all messages
+        // uid value is always included in FETCH response, envelope strings are in unicode.
+        for await (let message of client.fetch('1:*', { envelope: true })) {
+            console.log(`${message.uid}: ${message.envelope.subject}`);
+        }
+    } finally {
+        // Make sure lock is released, otherwise next `getMailboxLock()` never returns
+        lock.release();
+    }
+
+    // log out and close connection
+    await client.logout();
+};
+
+main().catch(err => console.error(err));
